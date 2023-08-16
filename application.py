c2a446e16e2cdf4a9bd3c9919e1816d649640d6a
Carlos Lugo <lugostein201@gmail.com>
2023-08-16 15:16:54 -0300

    Ya funciona el login
    Cree 3 cuentas para probar la autenticacion de credenciales:
    Acc: test pass: test
    Acc: test2 pass: test2
    Acc: test3 pass: test3
    Ya es capaz de identificar si ingresaste bien o no, falta pasar los datos al home nomas... Y corregir el bug de login del scroll

--- application.py
+++ application.py
@@ -1,6 +1,19 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 from config import DevConfig
 
+#Encriptar la pass
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
+#SQL
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="diocesis"
+)
 
 application = app = Flask(__name__)
 
@@ -12,9 +25,47 @@ app.config.from_object(DevConfig)
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == "POST":
+        usuario = request.form.get('usuario')
+        contrasena = request.form.get('contrasena')
+        print(f'Usuario: {usuario}, Contraseña: {contrasena}')
+        comprobarcuenta(usuario, contrasena)
     return render_template('login.html')
+
+def comprobarcuenta(user, pasw):
+    mycursor = mydb.cursor()
+    mycursor.execute('SELECT * FROM usuarios WHERE user = %(username)s', {'username': user})
+    data = mycursor.fetchall()
+    if data:
+      userdata = data[0]
+      if userdata:
+        passcheck = userdata[2]
+    if userdata and check_password_hash(passcheck, pasw):
+      print("Acceso concedido...")
+    else:
+      print("Malos credenciales...")
+
+#Este no se va a ver, ya que van a tener cuentas creadas por defecto ya..
+@app.route('/cargar_accs', methods=['POST', 'GET'])
+def cargar_accs():
+    usuario = request.form.get('usuario')
+    contrasena = request.form.get('contrasena')
+    if contrasena:
+        encriptada = createpassword(contrasena)
+        print(f'Usuario: {usuario}, Contraseña: {contrasena}, Encriptado {encriptada}')
+        mycursor = mydb.cursor()
+        mycursor.execute('INSERT INTO usuarios (user, password, id_tipo_user) VALUES (%s, %s, %s)',
+                        (usuario,
+                        encriptada, 1))
+        mydb.commit()
+        #Tipo usuario = 1 / Colegio
+        print("Creado...")
+    return render_template('cargar_accs.html')
+
+def createpassword(password):
+  return generate_password_hash(password)
 
 if __name__=='__main__':
     app.run(debug = True, port= 8000)
