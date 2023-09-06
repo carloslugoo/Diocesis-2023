621563ec341e16ae16c1cccaef03e6a8682221ec
Carlos Lugo <lugostein201@gmail.com>
2023-09-06 11:31:25 -0300

    Tipos de user
    Ya es capaz de identificar los tipos de user, admin o colegio, falta darle una navbar a cada uno..

--- application.py
+++ application.py
@@ -28,15 +28,27 @@ def index():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == "POST":
+        mycursor = mydb.cursor()
         usuario = request.form.get('usuario')
         contrasena = request.form.get('contrasena')
         print(f'Usuario: {usuario}, Contraseña: {contrasena}')
         aux = comprobarcuenta(usuario, contrasena)
-        if aux == True:
-          session['usuario'] = usuario
+        if aux:
+          print(aux)
+          #comprobar si es admin o alguien de una escuela
+          if int(aux[3]) == 2: #admin
+            query = "SELECT * FROM admin WHERE id_user = %s"
+            mycursor.execute(query, (int(aux[0]),)) 
+            data = mycursor.fetchall()
+          else: #colegio
+            query = "SELECT * FROM escuelas WHERE id_user = %s"
+            mycursor.execute(query, (int(aux[0]),)) 
+            data = mycursor.fetchall()
+          print(data)
+          session['user_data'] = data[0]
           return redirect(url_for('home'))
     return render_template('login.html')
-
+  
 def comprobarcuenta(user, pasw):
     mycursor = mydb.cursor()
     mycursor.execute('SELECT * FROM usuarios WHERE user = %(username)s', {'username': user})
@@ -47,14 +59,14 @@ def comprobarcuenta(user, pasw):
         passcheck = userdata[2]
       if userdata and check_password_hash(passcheck, pasw):
         print("Acceso concedido...")
-        return True
+        return userdata
     else:
       print("Malos credenciales...")
-      return False
+      return userdata
 
 @app.route('/home')
 def home():
-  usuario = session.get('usuario')
+  usuario = session.get('user_data')
   print(usuario)
   return render_template('home.html', user = usuario)
 #Este no se va a ver, ya que van a tener cuentas creadas por defecto ya..
